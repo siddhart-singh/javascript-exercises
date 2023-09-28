f8ec804c92f9ce9ca4e1b2811d98dbb7913511f7
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-29 00:34:20 +0530

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let clearedStr = clearStr(str);
+    for (let i = 0; i < clearedStr.length / 2; i++){
+        if (clearedStr[i] != clearedStr[(clearedStr.length - 1) - i]){
+            return false;
+        }
+    }
+    return true;
 };
+
+function clearStr(str){
+    let clearedStr = '';
+    for (let char of str){
+        if (isCharLetter(char)){
+            clearedStr += char.toLowerCase();
+        }
+    }
+   return clearedStr;
+}
+
+function isCharLetter(char){
+    return /^[a-z0-9]$/i.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+
+
+
+
+
+
