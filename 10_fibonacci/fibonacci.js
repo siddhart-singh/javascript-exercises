c511975f34acf0c2b1b63848dfdeb32fdd5be77b
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-29 00:52:20 +0530

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+   let last = 0;
+   let current = 1;
+   for (let i = 2; i <= num; i++){
+    [current, last] = [current + last, current];
+   }
+   return current;
 };
 
 // Do not edit below this line
