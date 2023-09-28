a5e07c27c6dfbf56fa2db6a23d448f2bb97ff616
siddhart-singh <siddhartsingh@hotmail.com>
2023-09-29 01:22:29 +0530

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.sort((next, last) => {
+    let [nextBirth, nextDeath] = [
+      next.yearOfBirth,
+      next.yearOfDeath ? next.yearOfDeath : new Date().getFullYear(),
+    ];
+    let [lastBirth, lastDeath] = [
+      last.yearOfBirth,
+      last.yearOfDeath ? last.yearOfDeath : new Date().getFullYear(),
+    ];
+    return lastDeath - lastBirth - (nextDeath - nextBirth);
+  })[0];
 };
 
 // Do not edit below this line
